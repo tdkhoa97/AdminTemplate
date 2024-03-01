d127c0e9fbdb9413dfd8de07318fd25aa140b791
Tran Khoa <tdkhoa97@gmail.com>
2024-03-01 14:21:06 +0700

    up

--- admin-page/src/App.tsx
+++ admin-page/src/App.tsx
@@ -62,36 +62,34 @@ function App() {
 
             </TooltipComponent>
           </div>
-          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-            {activeMenu ? (
-              <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-                <Sidebar />
-              </div>
-            ) : (
-              <div className="w-0 dark:bg-secondary-dark-bg">
-                <Sidebar />
-              </div>
-            )}
-            <div
-              className={
-                activeMenu
-                  ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                  : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-              }
-            >
-              <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-                <Navbar />
-              </div>
-              <div>
-                {themeSettings && (<ThemeSettings />)}
-                <Routes>
-                  {/* dashboard  */}
-                  <Route path="/" element={(<Transaction />)} />
-                  <Route path="/ecommerce" element={(<Transaction />)} />
-                </Routes>
-              </div>
-              <Footer />
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              <Sidebar />
             </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+            <div>
+              {themeSettings && (<ThemeSettings />)}
+              <Routes>
+                {/* dashboard  */}
+                <Route path="/" element={(<Transaction />)} />
+                <Route path="/ecommerce" element={(<Transaction />)} />
+              </Routes>
+            </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter >
